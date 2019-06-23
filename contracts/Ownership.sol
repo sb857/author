efbc15cfd1d667f4b54e26abc1f5f3fa1005f451
Utsav Talwar <talwar.utsav@gmail.com>
2019-06-23 23:32:18 +0530

    Update Ownership.sol

--- contracts/Ownership.sol
+++ contracts/Ownership.sol
@@ -1,21 +1,44 @@
-pragma solidity 0.5.8;
+pragma solidity 0.5;
 
-contract Ownership {
-    struct FileMap 
-        {
-            uint timestamp;
-            string owner;
-        }
+contract ContentShare {
+
+    struct FileMap {
+
+        uint timestamp;
+        address owner;
+        string contentName;
+        string ownerName;
+        uint price;
+        // address[] customer;
+    }
+
+    struct Customer {
+
+        string customerName;
+        uint _balance;
+        // FileMap booksBought;
+    }
+
+    struct Owner {
+
+        string ownerName;
+        uint _balance;
+        // FileMap booksUploaded;
+    }
 
     mapping (string => FileMap) allFiles;
+    mapping (address => Customer) customerDetails;
+    mapping (address => Owner) ownerDetails;
 
-    event FileLogStatus(bool status, uint timestamp, string owner, string ipfsHash);
+    event FileLogStatus(bool status, uint timestamp, address owner, string ipfsHash);
+    event BookPurchase(uint customerBalance, uint ownerBalance);
+    event TokenPurchase(uint customerBalance, address tokenBuyer);
 
-    function set(string memory owner, string memory ipfsHash) public
-    {
+    function uploadContent(address owner, string memory ipfsHash, string memory contentName, string memory ownerName, uint price) public {
         if(allFiles[ipfsHash].timestamp == 0)
+
         {
-            allFiles[ipfsHash] = FileMap(block.timestamp, owner);
+            allFiles[ipfsHash] = FileMap(block.timestamp, owner, contentName, ownerName, price);
             emit FileLogStatus(true, block.timestamp, owner, ipfsHash);
         }
 
@@ -25,8 +48,57 @@ contract Ownership {
         }
     }
 
-    function get(string memory ipfsHash) public returns (uint timestamp, string memory owner)
-    {
-        return (allFiles[ipfsHash].timestamp, allFiles[ipfsHash].owner);
+    function search(string memory fileHash) public view returns (uint timestamp, address owner) {
+        return (allFiles[fileHash].timestamp, allFiles[fileHash].owner);
     }
+
+    function purchase(string memory fileHash, string memory custName) public {
+
+        address owner = allFiles[fileHash].owner;
+        uint unitPrice = allFiles[fileHash].price;
+        uint custBalance;
+        uint ownerBalance;
+        
+        Owner memory author = ownerDetails[owner];
+        Customer memory customer = customerDetails[msg.sender];
+        customer.customerName = custName;
+        
+        custBalance = customer._balance - unitPrice;
+        ownerBalance = author._balance + unitPrice;
+
+        ownerDetails[owner] = Owner(author.ownerName, ownerBalance);
+        customerDetails[msg.sender] = Customer(custName, custBalance);
+
+        emit BookPurchase(customer._balance, author._balance);
+    }
+
+    function buyTokens(string memory name, uint tokens) public {
+        
+        Customer memory customer = customerDetails[msg.sender];
+        uint totalBalance = customer._balance + tokens;
+                    
+        customerDetails[msg.sender] = Customer(name, totalBalance);
+
+        emit TokenPurchase(customer._balance, msg.sender);
+        
+    }
+    
+    function getCustomer(address custAddress) public view returns (string memory customerName, uint customerBalance) {
+        return (customerDetails[custAddress].customerName, customerDetails[custAddress]._balance);
+    }
+    
+    // function getBooks() public view returns(string memory bookName) {
+    //     return();
+    // }
+    
+    // function getBookCustomers(string memory ipfsHash) public view returns (Customer memory customer){
+
+    //     return (allFiles[ipfsHash].customers);
+
+    // }
+
+    // function getBooks(address memory customerAddress) public view returns (FileMap fileMap) {
+    //     return()
+    // }
 }
+
