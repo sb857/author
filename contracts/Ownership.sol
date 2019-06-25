a6b67f74620f0bb074e59bcca30ca24734907943
Shrey Baid <43691976+sb857@users.noreply.github.com>
2019-06-25 14:40:57 +0530

    Update Ownership.sol

--- contracts/Ownership.sol
+++ contracts/Ownership.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
 
 contract ContentShare {
+    uint filecount;
 
     struct FileMap {
 
@@ -27,6 +30,7 @@ contract ContentShare {
     }
 
     mapping (string => FileMap) allFiles;
+    mapping (uint => string) allHashes;
     mapping (address => Customer) customerDetails;
     mapping (address => Owner) ownerDetails;
 
@@ -39,12 +43,14 @@ contract ContentShare {
     // string[] public foundIpfs;
     
     function uploadContent(string memory ipfsHash, string memory contentName, string memory ownerName, uint price) public {
+        
         if(allFiles[ipfsHash].timestamp == 0)
 
         {
             allFiles[ipfsHash] = FileMap(block.timestamp, msg.sender, contentName, ownerName, price);
             emit FileLogStatus(true, block.timestamp, msg.sender, ipfsHash);
-            
+            allHashes[filecount] = ipfsHash;
+            filecount++;
             hashValues.push(ipfsHash) -1;
             authors.push(ownerName) -1;
         }
@@ -98,9 +104,6 @@ contract ContentShare {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))) );
     }
     
-    // function getAllBooks() public view returns (string[] memory) {
-    //     return ();
-    // }
     
     function getBooks(string memory bookName) public view returns (string memory) {
         for(uint i=0; i<=hashValues.length; i++){
@@ -112,32 +115,12 @@ contract ContentShare {
             // return (foundIpfs);
         }
     }
+    
+    function getAllBooks() public view returns (FileMap[] memory) {
+        FileMap[] memory ret = new FileMap[](filecount);
+        for ( uint i = 0; i < filecount; i++){
+            ret[i] = allFiles[allHashes[i]];
+        }
+        return ret;
+    }
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
