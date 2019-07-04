9e4a4121ab738870ffbcf6bb37266ba005596671
Utsav Talwar <talwar.utsav@gmail.com>
2019-07-04 19:59:15 +0530

    Total Books Per Owner

--- contracts/Ownership.sol
+++ contracts/Ownership.sol
@@ -2,10 +2,9 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract ContentShare {
-    uint filecount;
 
     struct FileMap {
-
+        
         uint timestamp;
         address owner;
         string contentName;
@@ -14,27 +13,24 @@ contract ContentShare {
         uint priceRent;
         uint rentDays;
         string image;
+        uint totalBought;
     }
 
     struct Customer {
-
+        
         uint balance;
         string[][] booksBought;
-        // string[] booksBoughtName;
         string[][] booksRent;
-        // string[] booksRentName;
+        string[] uploadHash;
         mapping (string => rentAgreement) rentDetails;
     }
 
     struct rentAgreement {
-
+        
         uint boughtOn;
         uint validFor;
     }
     
-    mapping (string => FileMap) allFiles;
-    mapping (address => Customer) customerDetails;
-
     event FileLogStatus(bool status, uint timestamp, address owner, string ipfsHash);
     event BookPurchase(uint customerBalance, uint ownerBalance);
     event TokenPurchase(uint customerBalance, address tokenBuyer);
@@ -44,39 +40,52 @@ contract ContentShare {
     string[] public bookNames;
     string[] public prices;
     string[][] public bookDetails;  
+    string[][] public uploadedBooks;
 
+    
+    mapping (string => FileMap) allFiles;
+    mapping (address => Customer) customerDetails;    
+    
     function uploadContent(string memory ipfsHash, string memory contentName, string memory ownerName, uint price, uint rentPrice, uint rentDays, string memory image) public {
+        
+        Customer storage customer = customerDetails[msg.sender];
         
         if(allFiles[ipfsHash].timestamp == 0)
         {
-            allFiles[ipfsHash] = FileMap(block.timestamp, msg.sender, contentName, ownerName, price, rentPrice, rentDays, image);
-            emit FileLogStatus(true, block.timestamp, msg.sender, ipfsHash);
-            // allHashes[filecount] = ipfsHash;
-            // filecount++;
+            allFiles[ipfsHash] = FileMap(block.timestamp, msg.sender, contentName, ownerName, price, rentPrice, rentDays, image, 0);
+            customer.uploadHash.push(ipfsHash);
             hashValues.push(ipfsHash) -1;
-            // authors.push(ownerName) -1;
-            // bookNames.push(contentName) -1;
-            // prices.push(uint2str(price)) -1;
-            bookDetails.push([ownerName, contentName, uint2str(price), ipfsHash, image]) ;
+            bookDetails.push([ownerName, contentName, uint2str(price), ipfsHash, image]);
+            
+            emit FileLogStatus(true, block.timestamp, msg.sender, ipfsHash);
         }
-
         else
         {
             emit FileLogStatus(true, block.timestamp, msg.sender, ipfsHash);
         }
     }
 
-    function search(string memory fileHash) public view returns (string memory contentName) {
+    function searchName(string memory fileHash) public view returns (string memory contentName) {
+        
         return (allFiles[fileHash].contentName);
     }
-
-    function purchase(string memory fileHash, string memory custName) public {
+    
+    function searchTotal(string memory fileHash) public view returns (uint totalBought) {
+        
+        return (allFiles[fileHash].totalBought);
+    }
+    
+    function getUploads() public view returns (string[][] memory books) {
+        return(uploadedBooks);
+    }
+    
+    function purchase(string memory fileHash, string memory bookImage) public {
 
         address owner = allFiles[fileHash].owner;
         uint unitPrice = allFiles[fileHash].priceBuy;
         uint custBalance;
         uint ownerBalance;
-
+        
         Customer storage author = customerDetails[owner];
         Customer storage customer = customerDetails[msg.sender];
         
@@ -86,12 +95,14 @@ contract ContentShare {
         
         author.balance = ownerBalance;
         customer.balance = custBalance;
-        customer.booksBought.push([fileHash,search(fileHash)]) -1;
-        // customer.booksBoughtName.push(search(fileHash)) -1;
+        
+        allFiles[fileHash].totalBought++;
+
+        customer.booksBought.push([fileHash, allFiles[fileHash].contentName, bookImage]) -1;
         emit BookPurchase(customer.balance, author.balance);
     }
     
-    function rentTransaction(string memory fileHash) public {
+    function rentTransaction(string memory fileHash, string memory bookImage) public {
         
         uint currentTime = block.timestamp;
         address owner = allFiles[fileHash].owner;
@@ -100,10 +111,9 @@ contract ContentShare {
         uint rentPrice = allFiles[fileHash].priceRent;
         uint custBalance;
         uint ownerBalance;
-        
+        string memory hash = fileHash;
         Customer storage author = customerDetails[owner];
         Customer storage customer = customerDetails[msg.sender];
-        
         
         require(customer.balance >= rentPrice, "Low Wallet Balance");
         custBalance = customer.balance - rentPrice;
@@ -111,12 +121,14 @@ contract ContentShare {
         
         author.balance = ownerBalance;
         customer.balance = custBalance;
-        customer.booksRent.push([fileHash,search(fileHash)]) -1;
-        // customer.booksRentName.push(search(fileHash)) -1;
+        
+        allFiles[fileHash].totalBought++;
+
+        customer.booksRent.push([hash, allFiles[hash].contentName, bookImage]);
         customer.rentDetails[fileHash] = rentAgreement(currentTime, finalTime);
     }
     
-    function getRentUpdate(address customer, string memory fileHash) public view returns (bool) {
+    function getRentUpdate(string memory fileHash) public view returns (bool) {
         
         Customer storage customer = customerDetails[msg.sender];
         if(now == customer.rentDetails[fileHash].validFor) {
@@ -133,7 +145,7 @@ contract ContentShare {
     }
     
     function getCustomer(address custAddress) public view returns (uint customerBalance, string[][] memory boughtBooks, string[][] memory rentBooks) {
-        return (customerDetails[custAddress].balance, customerDetails[custAddress].booksBought, customerDetails[custAddress].booksRent );
+        return (customerDetails[custAddress].balance, customerDetails[custAddress].booksBought, customerDetails[custAddress].booksRent);
     }
     
     function compareStrings (string memory a, string memory b) public view returns (bool) {
@@ -158,6 +170,16 @@ contract ContentShare {
         for(uint i =0; i< customer.booksBought.length; i++) {
             if(compareStrings(ipfsHash, customer.booksBought[0][i])) {
                 return true;
+            }
+        }
+    }
+    
+    function getBookTotal(string memory ipfsHash) public view returns (uint totalBought) {
+        
+        Customer storage customer = customerDetails[msg.sender];
+        for(uint i=0; i<= customer.uploadHash.length; i++) {
+            if(compareStrings(customer.uploadHash[i],ipfsHash)) {
+                return totalBought;
             }
         }
     }
