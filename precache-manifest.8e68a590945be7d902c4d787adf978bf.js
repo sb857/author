ceb2eafd77a9b0e5d2b75fbeaa57970a566723d7
Talwar <utsav.talwar@capgemini.com>
2019-07-09 15:55:52 +0530

    Updates

---
+++ precache-manifest.8e68a590945be7d902c4d787adf978bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82e9bc5a21458d1125cda206f3768a0",
+    "url": "/auth.or/index.html"
+  },
+  {
+    "revision": "80662462b37f13f731f7",
+    "url": "/auth.or/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "10118c65dba87e68bb02",
+    "url": "/auth.or/static/css/main.f68ac188.chunk.css"
+  },
+  {
+    "revision": "80662462b37f13f731f7",
+    "url": "/auth.or/static/js/2.53dcd0e7.chunk.js"
+  },
+  {
+    "revision": "10118c65dba87e68bb02",
+    "url": "/auth.or/static/js/main.38ca5cb0.chunk.js"
+  },
+  {
+    "revision": "58c47304851577694652",
+    "url": "/auth.or/static/js/runtime~main.99bdf044.js"
+  }
+]);
